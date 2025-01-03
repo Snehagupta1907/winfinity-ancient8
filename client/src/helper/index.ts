4e74579357ada3a11df4cddf7e398c36b4944cf5
sneha gupta <snehagupta@snehas-MacBook-Pro.local>
2025-01-03 23:11:03 +0530

    upadte

--- client/src/helper/index.ts
+++ client/src/helper/index.ts
@@ -1,6 +1,6 @@
 import { Chain } from '@rainbow-me/rainbowkit';
 import { uxuyWallet } from '@/wallets/uxuyWallet';
-import { ancient8Sepolia } from 'wagmi/chains';
+import { opBNBTestnet, mainnet,flowTestnet,ancient8Sepolia } from 'wagmi/chains';
 import {
   connectorsForWallets,
 } from '@rainbow-me/rainbowkit';
@@ -11,7 +11,7 @@ import {
   walletConnectWallet,
   okxWallet,
 } from '@rainbow-me/rainbowkit/wallets';
-const chains: readonly [Chain, ...Chain[]] = [ancient8Sepolia];
+const chains: readonly [Chain, ...Chain[]] = [ancient8Sepolia, opBNBTestnet,flowTestnet,mainnet];
 const connectors = connectorsForWallets(
   [
     {
@@ -30,5 +30,9 @@ export const config = createConfig({
   chains: chains,
   transports: {
     [ancient8Sepolia.id]: http('https://rpcv2-testnet.ancient8.gg'),
+    [opBNBTestnet.id]: http('https://opbnb-testnet-rpc.bnbchain.org/'),
+    [mainnet.id]: http('https://bsc-dataseed.binance.org/'),
+    [flowTestnet.id]: http('https://testnet.evm.nodes.onflow.org/'),
+    
   },
 });
